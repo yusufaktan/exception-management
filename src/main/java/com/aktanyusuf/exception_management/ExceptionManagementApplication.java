87d9a1ff30f7bc90d519134f96a52cf3ac0b3e06
Yusuf Aktan <yusufaktn@outlook.com>
2024-11-29 00:28:48 +0300

    spring init

---
+++ src/main/java/com/aktanyusuf/exception_management/ExceptionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.exception_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktanyusuf/exception_management/ExceptionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.exception_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
